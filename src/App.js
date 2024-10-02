b78a4f36a4b21ed9db3d49b23bfc696b565eca46
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2024-10-02 07:24:46 +0530

    smoke tree assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from "react"
+import axios from "axios"
+import Header from "./components/Header"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {name:'',userAddress:''}
+
+  changeUsername = event => {
+    this.setState({name:event.target.value})
+  }
+
+  changeAddress = event => {
+    this.setState({userAddress:event.target.value})
+  }
+  onSubmitForm = async(event) => {
+    const {name,userAddress} = this.state
+    event.preventDefault()
+    const userDetails = {name,userAddress}
+    await axios.post("http://localhost:3004/register",{userDetails})
+  }
+  render(){
+    const {name,userAddress} = this.state
+    return (
+      <div> 
+      <Header/>
+      <div className="main-container">
+        <form onSubmit={this.onSubmitForm} action="POST" className="form-container">
+         <h2 className="title">Let's Work<br/><span className="sub-title">Together</span></h2>
+        <div>
+          <p className="label-names">Name:</p>
+          <input type="text" className="user-name" placeholder="Username" onChange={this.changeUsername} value={name} required/>
+          </div>
+          <div>
+          <p className="label-names">Address:</p>
+          <textarea cols={30} rows={5} placeholder="Type address here..." onChange={this.changeAddress} value={userAddress} className="user-name" required/>
+          </div>
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
+      </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import "./index.css"
+
+const Header = () => (
+    <div className="header-container">
+        <img src="https://res.cloudinary.com/dksgsqhdk/image/upload/v1727790676/smoketreeslogo_dwjvim.png" alt="logo" className="logo-image"/>
+        <ul className="services-list">
+          <li>About</li>
+          <li>Products</li>
+          <li>Services</li>
+        </ul>
+      </div>
+)
+
+export default Header
